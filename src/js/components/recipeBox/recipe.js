c48e6d2f0726627e16cd79ea9f057c4a17223fa0
jktravis <joshua.travis@gmail.com>
2016-04-13 19:44:36 -0400

    Added modals

--- src/js/components/recipeBox/recipe.js
+++ src/js/components/recipeBox/recipe.js
@@ -15,18 +15,43 @@ var Recipe = React.createClass({
             <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
                href={"#collapse" + this.props.label} aria-expanded="false"
                aria-controls={"collapse" + this.props.label}>
-              Collapsible Group Item #{this.props.label}
+              Key Lime Pie #{this.props.label}
             </a>
+            <button className="btn btn-sm btn-link" data-toggle="modal" data-target="#myModal">
+              <span className="glyphicon glyphicon-pencil text-muted"/>
+            </button>
+            <button className="btn btn-sm btn-link" data-toggle="modal" data-target="#myModal">
+              <span className="glyphicon glyphicon-trash text-muted"/>
+            </button>
           </h4>
         </div>
         <div id={"collapse" + this.props.label} className="panel-collapse collapse" role="tabpanel"
              aria-labelledby={"heading" + this.props.label}>
           <ul className="list-group">
-            <li className="list-group-item">food</li>
-            <li className="list-group-item">food</li>
-            <li className="list-group-item">food</li>
-            <li className="list-group-item">food</li>
+            <li className="list-group-item">Keys</li>
+            <li className="list-group-item">Limes</li>
+            <li className="list-group-item">Pies</li>
+            <li className="list-group-item">Crust</li>
           </ul>
+
+        </div>
+        <div className="modal fade" id="myModal" role="dialog" aria-labelledby="myModalLabel">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 className="modal-title" id="myModalLabel">Modal title</h4>
+              </div>
+              <div className="modal-body">
+                ...
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
