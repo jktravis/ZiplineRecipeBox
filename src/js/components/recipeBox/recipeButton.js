a46938bcf92d42bc1ad6b7b5b7b5843d6ed79f2b
jktravis <joshua.travis@gmail.com>
2016-04-13 09:19:51 -0400

    Added Google font, a background, and some color.

--- src/js/components/recipeBox/recipeButton.js
+++ src/js/components/recipeBox/recipeButton.js
@@ -5,7 +5,7 @@ var React = require('react');
 var RecipeButton = React.createClass({
   render: function render() {
     return (
-      <button className="btn btn-primary btn-lg">Add Recipe</button>
+      <button className="btn btn-warning btn-lg">Add Recipe</button>
     );
   }
 });
