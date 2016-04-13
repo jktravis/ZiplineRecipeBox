826cca55693c5310a8127a91709f43dc0cba4dd7
jktravis <joshua.travis@gmail.com>
2016-04-13 11:55:17 -0400

    Swapped out <ul> for Bootstrap accordion and list groups.

--- src/js/components/recipeBox/recipe.js
+++ src/js/components/recipeBox/recipe.js
@@ -2,12 +2,37 @@
 
 var React = require('react');
 
-var RecipeListItem = React.createClass({
+var Recipe = React.createClass({
+  propTypes: {
+    label: React.PropTypes.number.isRequired
+  },
+
   render: function render() {
     return (
-      <li>List Item</li>
+      <div className="panel panel-default">
+      <div className="panel-heading" role="tab" id={"heading" + this.props.label}>
+          <h4 className="panel-title">
+            <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+               href={"#collapse" + this.props.label} aria-expanded="false"
+               aria-controls={"collapse" + this.props.label}>
+              Collapsible Group Item #{this.props.label}
+            </a>
+          </h4>
+        </div>
+        <div id={"collapse" + this.props.label} className="panel-collapse collapse" role="tabpanel"
+             aria-labelledby={"heading" + this.props.label}>
+          <ul className="list-group">
+            <li className="list-group-item">food</li>
+            <li className="list-group-item">food</li>
+            <li className="list-group-item">food</li>
+            <li className="list-group-item">food</li>
+          </ul>
+          <button className="btn btn-danger btn-sm">Delete</button>
+          <button className="btn btn-default btn-sm">Edit</button>
+        </div>
+      </div>
     );
   }
 });
 
-module.exports = RecipeListItem;
+module.exports = Recipe;
--- src/js/components/recipeBox/recipeList.js
+++ src/js/components/recipeBox/recipeList.js
@@ -6,9 +6,11 @@ var Recipe = require('./recipe');
 var RecipeList = React.createClass({
   render: function render() {
     return (
-      <ul>
-        <Recipe/>
-      </ul>
+      <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+        <Recipe label={1}/>
+        <Recipe label={2}/>
+        <Recipe label={3}/>
+      </div>
     );
   }
 });
