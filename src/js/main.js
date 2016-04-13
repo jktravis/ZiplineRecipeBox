022e78e7b182f151038c51706878efa3f5c4467b
jktravis <joshua.travis@gmail.com>
2016-04-13 19:07:01 -0400

    Enabled x-editable.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,17 @@ require('bootstrap/dist/css/bootstrap.min.css');
 require('bootstrap/dist/css/bootstrap-theme.min.css');
 require('bootstrap');
 require('../css/main.scss');
+require('../../vendor/x-editable/bootstrap3-editable/css/bootstrap-editable.css');
+require('../../vendor/x-editable/bootstrap3-editable/js/bootstrap-editable.min');
+
+$.fn.editable.defaults.mode = 'inline';
+
+$(document).ready(function () {
+  $('.list-group-item').editable({
+    type: 'text',
+    title: 'Edit Item'
+  });
+});
 
 var RecipeBox = require('./components/recipeBox/recipeBox');
 
