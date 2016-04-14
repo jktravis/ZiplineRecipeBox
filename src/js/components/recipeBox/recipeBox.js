118b09ec55500c12824ed5c14020b39790df279a
jktravis <joshua.travis@gmail.com>
2016-04-14 15:52:03 -0400

    Init with sample data; Setup downstream data flow.

--- src/js/components/recipeBox/recipeBox.js
+++ src/js/components/recipeBox/recipeBox.js
@@ -5,6 +5,38 @@ var RecipeButton = require('./recipeButton');
 var RecipeList = require('./recipeList');
 
 var RecipeBox = React.createClass({
+  getInitialState: function () {
+    var storage = window.localStorage;
+    var keys = Object.keys(storage);
+    var data = {recipes: []};
+    // some sample data to get things started.
+    if (keys.length === 0) {
+      let initialData = [
+        {
+          name: "Key Lime Pie",
+          ingredients: ['Keys', 'Limes', 'Pies']
+        },
+        {
+          name: "Meatloaf",
+          ingredients: ['Meat', 'Loaf', 'Ketchup']
+        },
+        {
+          name: "Ham Sandwich",
+          ingredients: ['Ham', 'Cheese', 'Bread']
+        }
+      ];
+
+      initialData.map(function (recipe) {
+        storage.setItem('jktravis_recipes_' + recipe.name, JSON.stringify(recipe));
+      });
+    }
+
+    for (var i = 0; i < keys.length; i++) {
+      data.recipes.push(JSON.parse(storage.getItem(keys[i])));
+    }
+    return data;
+  },
+
   render: function render() {
     return (
       <div className="container-fluid">
@@ -13,7 +45,8 @@ var RecipeBox = React.createClass({
         </div>
 
         <div className="col-lg-8 col-lg-offset-2 well row">
-          <RecipeList />
+          {console.log(this.state.recipes)}
+          <RecipeList recipes={this.state.recipes}/>
         </div>
 
         <div className="col-lg-8 col-lg-offset-2 row">
--- src/js/components/recipeBox/recipeList.js
+++ src/js/components/recipeBox/recipeList.js
@@ -4,12 +4,17 @@ var React = require('react');
 var Recipe = require('./recipe');
 
 var RecipeList = React.createClass({
+  propTypes: {
+    recipes: React.PropTypes.array.isRequired
+  },
+
   render: function render() {
+    var createRecipe = function (item) {
+      return (<Recipe name={item.name} ingredients={item.ingredients} label={item.name.replace(/\s+/g, '')}/>);
+    };
     return (
       <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-        <Recipe name="Key Lime Pie" ingredients={["Keys", "Limes", "Pies"]} label="KeyLimePie"/>
-        <Recipe name="Ham Sandwich" ingredients={["Ham", "Cheese", "Bread"]} label="HamSandwich"/>
-        <Recipe name="Meatloaf" ingredients={["Meat", "Loaf", "Ketchup"]} label="Meatloaf"/>
+        {this.props.recipes.map(createRecipe, this)}
       </div>
     );
   }
